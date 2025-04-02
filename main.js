9fefc9d44f107cacea1a7e5385f04f45e8604b3f
RyanM3 <ryangmurray3@gmail.com>
2025-04-02 12:55:43 -0500

    Card Flipper complete basic version

---
+++ main.js
@@ -0,0 +1,17 @@
+const card= document.querySelector('.card__inner');
+
+card.addEventListener('click', function () {
+    card.classList.toggle('is-flipped');
+})
+
+const card2= document.querySelector('.card__inner2');
+
+card2.addEventListener('click', function () {
+    card2.classList.toggle('is-flipped');
+})
+
+const card3= document.querySelector('.card__inner3');
+
+card3.addEventListener('click', function () {
+    card3.classList.toggle('is-flipped');
+})
